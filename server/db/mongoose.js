9fae641968e840bf828e044de3d2fa6449890390
orjerby <orjerby@gmail.com>
2019-02-21 15:51:39 +0200

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://orjerby:or2402@cluster0-dc2za.mongodb.net/test?retryWrites=true');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb+srv://orjerby:or2402@cluster0-dc2za.mongodb.net/test?retryWrites=true');
 
 module.exports = {
     mongoose
-}
+};
