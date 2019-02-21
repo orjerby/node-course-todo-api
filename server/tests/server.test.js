1a6b5f28be5f902656ad3cd4195abd216fcb1c13
orjerby <orjerby@gmail.com>
2019-02-21 12:12:15 +0200

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [{
+    text: 'First text todo'
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => { // preparing the database for the tests
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,7 +34,7 @@ describe('POST /todos', () => {
                     return done(err); // this return just stops everything
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -40,15 +48,27 @@ describe('POST /todos', () => {
             .post('/todos')
             .send({})
             .expect(400)
-            .end((err, res) => {
+            .end((err, res) => { // here we want to check more things on the database(async)
                 if (err) {
                     return done(err);
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((err) => done(err));
             });
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done); // we dont need to provide function to end because we're not doing anything async
     });
 });
