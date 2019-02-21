2af95da50d03e3cd3b507b6b125104f317bb59e8
orjerby <orjerby@gmail.com>
2019-02-21 14:55:04 +0200

    Fix mongodb atlas server string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://orjerby:<or2402>@cluster0-dc2za.mongodb.net/test?retryWrites=true');
+mongoose.connect('mongodb+srv://orjerby:or2402@cluster0-dc2za.mongodb.net/test?retryWrites=true');
 
 module.exports = {
     mongoose
