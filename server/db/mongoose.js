258ebb9b074a01a2b4616c936ccb3ad012379dfc
orjerby <orjerby@gmail.com>
2019-02-22 14:30:46 +0200

    Try fix heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
-// mongoose.connect('mongodb+srv://orjerby:or2402@cluster0-dc2za.mongodb.net/test?retryWrites=true');
+//mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb+srv://orjerby:1@cluster0-dc2za.mongodb.net/test?retryWrites=true');
 
 module.exports = {
     mongoose
